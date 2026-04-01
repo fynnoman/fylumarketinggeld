dd232ffd54708b99115ec97d052323185a025077
Fynn Schulz <finolino9@gmail.com>
2026-04-01 11:38:29 +0200

    fix: add /buchen/success to sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -23,6 +23,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.8,
     },
     {
+      url: `${baseUrl}/buchen/success`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.3,
+    },
+    {
       url: `${baseUrl}/impressum`,
       lastModified: new Date(),
       changeFrequency: "yearly",
